09dbabf8f5aba4ac33fb7ee15cd4aae6f80e09f3
Kamil Sieniawski <kamil.sieniawski@gmail.com>
2019-02-24 23:22:46 +0100

    add TypeORM and dev database container

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,14 @@
 import './common/env';
+import "reflect-metadata";
+import { createConnection } from "typeorm";
 import Server from './common/server';
 import routes from './routes';
 
-const port = parseInt(process.env.PORT);
-export default new Server()
-  .router(routes)
-  .listen(port);
+
+createConnection().then(connection => {
+  const port = parseInt(process.env.PORT)
+
+  new Server()
+    .router(routes)
+    .listen(port)
+})
